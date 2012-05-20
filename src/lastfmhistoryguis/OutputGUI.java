55888662ebf4d093b26747d956ef4bd4fb45de48
Chris Brammeier <wizard710@gmail.com>
2012-05-20 23:40:58 +0100

    Started to write the output graphics

--- src/lastfmhistoryguis/OutputGUI.java
+++ src/lastfmhistoryguis/OutputGUI.java
@@ -1,27 +1,38 @@
 package lastfmhistoryguis;
 
 import java.awt.Canvas;
+
+import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
+import de.umass.lastfm.Track;
 import lastfmhistoryclasses.LastFMHistory;
 
-public class OutputGUI extends Canvas{
+public class OutputGUI extends JPanel{
+	private final static int PAD = 20;
 
-	public OutputGUI(){
-		OutputGUI canvas = new OutputGUI();
-		JFrame frame = new JFrame();
-		frame.setSize(1200, 800);
-		frame.getContentPane().add(canvas);
-		frame.setVisible(true);
+	
+	public void paintComponent(Graphics g){
+		Graphics2D graph = (Graphics2D)g;
+		int w = 1200;
+		int h = 800;
+		int x0 = w - PAD;
+		graph.draw(new Rectangle2D.Double(PAD, PAD, w-2*PAD, h-2*PAD));
+		double xInc = (double)(w - 2*PAD)/(Math.abs(dayMax));
+		double secondHeight = (double)(h - 2*PAD)/86400;
+		for(Track t: history){
+			Color color = t.getColour();
+			int duration = t.getDuration();
+			int day = t.getDay();
+			
 		
+		}
 	}
-	public void paint(Graphics graphics){
-	}
-	
-	
-	
 	
 }
