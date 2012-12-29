9cf456391e1cf9afce0f09ac64ff04f897d36b10
Chris Simon <wizard710@gmail.com>
2012-12-29 15:58:19 +0000

    Removed whitespace and misc, unused imports etc

--- src/lastfmhistoryclasses/MainClass.java
+++ src/lastfmhistoryclasses/MainClass.java
@@ -3,36 +3,25 @@ package lastfmhistoryclasses;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 
-import javax.swing.JFrame;
-import lastfmhistoryguis.*;
-
-	
-
 public class MainClass {
-	
 	
 	public static int screenHeight;
 	public static int screenWidth;
 	public static final int SCREENPAD = 10;
 	
-	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Toolkit tk = Toolkit.getDefaultToolkit();
 		Dimension d = tk.getScreenSize();
 		screenHeight = (int) d.getHeight() - 2 * SCREENPAD - 50;
 		screenWidth = (int) d.getWidth() - 2 * SCREENPAD;
 		
 		System.out.println("Main Class:: Width = " + screenWidth + ", Height = " + screenHeight);
-		
-		
+		@SuppressWarnings("unused")
 		Controller controller = new Controller(screenWidth, screenHeight);
 				
 	}
 	
-	
-
 }
